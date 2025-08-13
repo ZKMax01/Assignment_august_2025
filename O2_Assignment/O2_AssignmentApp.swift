78fadac7814c92f7f915cdc8a0f7428bfc9d9c1b
ZKMax01 <gitproject@zoltankoos.eu>
2025-08-13 13:18:51 +0200

    Initial Commit

---
+++ O2_Assignment/O2_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  O2_AssignmentApp.swift
+//  O2_Assignment
+//
+//  Created by ZKMax01 on 13/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct O2_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
